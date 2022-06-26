d29af08b6e84c31ee358e916a5657e32e8f388bf
m06lin <ylinda1122@gmail.com>
2022-06-26 17:45:22 +0800

    feat: 建立資料表-食材;類別;食譜 - 建立migrate - ingredients:食菜 - recipes:食譜 - dish_types:類別 - recipe_ingredient:食譜與食材多對多對應

---
+++ database/migrations/2022_06_20_075706_create_ingredients_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateIngredientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ingredients', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 50);
+            $table->unsignedInteger('calories')->comment('卡路里');
+            $table->unsignedInteger('quantity')->comment('數量');
+            $table->string('unit', 20)->comment('單位');
+            $table->float('amount')->comment('金額');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ingredients');
+    }
+}
---
+++ database/migrations/2022_06_20_080328_create_recipes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRecipesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('recipes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 50);
+            $table->unsignedBigInteger('dish_type_id');
+ 
+            $table->foreign('dish_type_id')->references('id')->on('dish_types');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('recipes');
+    }
+}
